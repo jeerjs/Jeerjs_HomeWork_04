6f976aee093e053cab89c1d3b32a5baf2ce80b32
Jaswant Jeers <jeerjaswant@yahoo.co.uk>
2022-04-29 13:31:45 +0100

    Dynamically Render to Question 1

--- Assets/script.js
+++ Assets/script.js
@@ -16,19 +16,42 @@ const renderQuestion = () => {
   h2.textContent = "Who founded the NHS?";
   //create ul and append 4 li
   const ul = document.createElement("ul");
-  //Create div and append button
-  const div = document.createElement("div");
+  ul.setAttribute("class", "feedback-list");
+
+  //call a function to create an li and append - loop
+  const li1 = document.createElement("li");
+  li1.setAttribute("class", "list-item");
+  li1.textContent = "Aneurin Bevan";
+
+  const li2 = document.createElement("li");
+  li2.setAttribute("class", "list-item");
+  li2.textContent = "Margaret Thatcher";
+
+  const li3 = document.createElement("li");
+  li3.setAttribute("class", "list-item");
+  li3.textContent = "Winston Churchill";
+
+  const li4 = document.createElement("li");
+  li4.setAttribute("class", "list-item");
+  li4.textContent = "Henry VIII";
+
+  ul.append(li1, li2, li3, li4);
+
+  section.append(h2, ul);
+
+  //append to main element
+  mainElement.append(section);
 };
 
 //remove banner
 const removeBanner = () => {
-  console.log(removeBanner);
   bannerSection.remove();
 };
 
 //declare the event handler function for start button click
 const handleStartButtonClick = () => {
   removeBanner();
+  renderQuestion();
 };
 
 //remove banner section on click and render first question
