4494963c06adf16d1c29d608ba31690e550e81e1
Jaswant Jeers <jeerjaswant@yahoo.co.uk>
2022-05-03 00:30:59 +0100

    Added live score table

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,117 @@
+//Li not working
+
+//create a table via javascript that hold data passed from local storage
+//identify correct answers and assign a value
+//pass value to local storage
+//display in table
+//clear results button
+//change feedback to just score
+
 const startButton = document.getElementById("start-btn");
 
 const bannerSection = document.getElementById("bannersectio");
+
+const mainElement = document.getElementById("main");
+
+const clearbutton = document.createElement("button");
+
+const button2 = document.createElement("button");
+
+// const handleclearbutton = () => {
+//   location.href = "https://jeerjs.github.io/Jeerjs_HomeWork_04/";
+// };
+
+var score = 0;
 
 //current question index
 let questionindex = 0;
 
 const questions = [
   {
-    text: "Question 1",
-    options: ["yes", "No", "Maybe"],
+    text: "What is the most commonly dispensed drug in Pharmacy",
+    options: ["Paracetamol", "Amlodipine", "Simvastatin", "Betahistine"],
+    answer: "Paracetamol",
   },
   {
-    text: "Question 2",
-    options: ["yes", "No", "Maybe"],
+    text: "How many flu vaccinations do pharmacies in England do each year",
+    options: ["500 Thousand", "1 million", "3 million", "4 million"],
+    answer: "4 Million",
   },
   {
-    text: "Question 3",
-    options: ["yes", "No", "Maybe"],
+    text: "Who regulates pharmacies in England",
+    options: ["RPS", "GPHC", "PSNC", "APPG"],
+    answer: "GPHC",
   },
   {
-    text: "Question 4",
-    options: ["yes", "No", "Maybe"],
+    text: "Which of these services does pharmacy do in England",
+    options: [
+      "Referall to Hospitals",
+      "Diabetic Foot Check",
+      "Blood Pressure Screening",
+      "HIV screening",
+    ],
+    answer: "Blood Pressure Screening",
   },
   {
-    text: "Question 5",
-    options: ["yes", "No", "Maybe"],
+    text: "What is the most common category of medicine for minor illess treated over the counter",
+    options: ["Constipation", "Pain", "Atheletes Foot", "Coughs/Colds"],
+    answer: "Pain",
   },
 ];
+var timeEl = document.querySelector(".time");
+// Selects element by id
+var mainEl = document.getElementById("mainn");
+var secondsLeft = 60;
+var score = 0;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+    // const getCat = function (obj) {};
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+  }, 1000);
+}
+
+function sendMessage() {
+  alert("Time Has Run Out!");
+  removeQuestion();
+  document.getElementById("feedback-form").remove();
+}
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  //get full name from input
+  const fullName = document.getElementById("full-name").value;
+  ///validiate
+  if (fullName) {
+    const feedbackResults = JSON.parse(localStorage.getItem("feedbackResults"));
+    //build object with fullname and results
+    const result = {
+      fullName,
+      score,
+    };
+    //push back to local storage
+    storeInLS("allResults", result);
+
+    //clear results in local storage
+    localStorage.removeItem("feedbackResults");
+
+    //remove submit form.
+    document.getElementById("feedback-form").remove();
+
+    highscores();
+  } else {
+    alert("Please enter your name");
+  }
+};
 
 const handleoptionclick = (event) => {
   //getcurrent target
@@ -36,22 +121,28 @@ const handleoptionclick = (event) => {
   console.log(currenttarget);
   console.log(target.tagName);
 
-  if (target.tagName === "LI") {
+  if (target.tagName == "LI") {
     //get option user clicked
     const value = target.getAttribute("data-value");
     console.log(value);
 
     //get the question user clicked
     const question = questions[questionindex].text;
+    const answer = questions[questionindex].answer;
     console.log(question);
+    if (answer === target.textContent) {
+      score += 10;
+    } else {
+      secondsLeft -= 10;
+    }
 
-    //build the answer object and answer
-    const answer = {
-      question,
-      value,
-    };
+    // //build the answer object and answer
+    // const answer = {
+    //   question,
+    //   value,
+    // };
     //Store answer in local storage
-    storeAnswerInLS(answer);
+    storeInLS("feedbackResults", answer);
   }
 
   // go to next question
@@ -62,17 +153,125 @@ const handleoptionclick = (event) => {
     renderQuestion();
   } else {
     //if last question then render results
-    renderResults();
-    renderform();
+    // renderResults();
+    renderForm();
     //remove previous questions
   }
 
   const renderResults = () => {
     console.log("render results");
   };
-  const renderForm = () => {
-    console.log("render form");
-  };
+};
+
+//fucntion to clear allResults from localstorage on click
+//function to go back to home screen
+
+//render the highscores page--------------------------------
+const highscores = () => {
+  console.log("highscores");
+  const section = document.createElement("section");
+  section.setAttribute("id", "bannersectio");
+  section.setAttribute("id", "feedback-form");
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "title");
+  h2.textContent = "Your Score is " + score + " out of 40";
+  const h3 = document.createElement("h3");
+  h3.setAttribute("class", "title");
+  const h1 = document.createElement("h1");
+  h1.setAttribute("class", "title");
+  h1.textContent = "High Scores Table";
+  const table2 = document.createElement("highscorestable");
+  // const Table1 = document.createElement("Table1");
+  // const tr = document.createElement("tr");
+  // const tdname = document.createElement("tdname");
+  // const tdscore = document.createElement("tdscore");
+
+  const buttonDiv = document.createElement("div");
+  const buttondiv2 = document.createElement("div");
+  buttonDiv.setAttribute("class", "form-control");
+  buttondiv2.setAttribute("class", "form-control");
+  button2.setAttribute("type", "submit");
+  button2.setAttribute("class", "btn");
+  // button2.textContent = "Clear Scores";
+  clearbutton.setAttribute("type", "submit");
+  clearbutton.setAttribute("class", "btn");
+  clearbutton.textContent = "Play Again";
+  button2.textContent = "Clear Scores";
+
+  buttonDiv.append(button2);
+  buttondiv2.append(clearbutton);
+  section.append(h1, h2, h3, table2, buttonDiv, buttondiv2);
+
+  mainElement.append(section);
+};
+
+function handleclearbutton() {
+  window.location.assign("https://jeerjs.github.io/Jeerjs_HomeWork_04/");
+}
+
+function Handlereset() {
+  localStorage.clear();
+  console.log("I am working");
+}
+var retrievedScores = JSON.parse(localStorage.getItem("allResults"));
+var hst = document.getElementById("highscorestable");
+
+for (var i = 0; i < retrievedScores.length; i++) {
+  hst.innerHTML +=
+    "<tr><td>" +
+    retrievedScores[i].fullName +
+    "</td><td>" +
+    retrievedScores[i].score +
+    "</td></tr>";
+}
+
+//define coirrect anseer and assign value and pass to local storage---
+
+//pass results from local storage to table in highscores page-------------------------------------
+
+//function for timer - deduect when wrong answer.
+
+//function to render the form
+const renderForm = () => {
+  console.log("render form");
+  const section = document.createElement("section");
+  section.setAttribute("class", "feedback-form-section");
+  section.setAttribute("id", "feedback-form");
+
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "title");
+  h2.textContent = "Submit your feedback";
+
+  const form = document.createElement("form");
+
+  const inputDiv = document.createElement("div");
+  inputDiv.setAttribute("class", "form-control");
+
+  const input = document.createElement("input");
+  input.setAttribute("id", "full-name");
+  input.setAttribute("class", "form-input");
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", "Enter full name");
+
+  inputDiv.append(input);
+
+  const buttonDiv = document.createElement("div");
+  buttonDiv.setAttribute("class", "form-control");
+
+  const button = document.createElement("button");
+  button.setAttribute("type", "submit");
+  button.setAttribute("class", "btn");
+  button.textContent = "Submit";
+
+  buttonDiv.append(button);
+
+  form.append(inputDiv, buttonDiv);
+
+  section.append(h2, form);
+
+  mainElement.append(section);
+  // add event listener for form submission
+  form.addEventListener("submit", handleFormSubmit);
 };
 
 //function to render question to page
@@ -136,13 +335,13 @@ const removeBanner = () => {
   bannerSection.remove();
 };
 
-const storeAnswerInLS = (answer) => {
+const storeInLS = (key, value) => {
   //get feeback results from LS
-  const feedbackResults = JSON.parse(localStorage.getItem("feedbackResults"));
+  const arrayFromLS = JSON.parse(localStorage.getItem(key));
   //push answer to array
-  feedbackResults.push(answer);
+  arrayFromLS.push(value);
   //set feebackresult in LS
-  localStorage.setItem("feedbackResults", JSON.stringify(feedbackResults));
+  localStorage.setItem(key, JSON.stringify(arrayFromLS));
 };
 const initialiseLocalStorage = () => {
   //get feedback from local strpage
@@ -150,9 +349,16 @@ const initialiseLocalStorage = () => {
     localStorage.getItem("feedbackResults")
   );
 
+  const allResultsFromLS = JSON.parse(localStorage.getItem("allResults"));
+
   if (!feedbackResultsFormLS) {
     //if not exsist create empty array
     localStorage.setItem("feedbackResults", JSON.stringify([]));
+  }
+
+  if (!allResultsFromLS) {
+    //if not exsist create empty array
+    localStorage.setItem("allResults", JSON.stringify([]));
   }
 };
 
@@ -162,6 +368,11 @@ const handleStartButtonClick = () => {
   initialiseLocalStorage();
   renderQuestion();
   removeBanner();
+  setTime();
 };
 
+//
+
 startButton.addEventListener("click", handleStartButtonClick);
+clearbutton.addEventListener("click", handleclearbutton);
+button2.addEventListener("click", Handlereset);
